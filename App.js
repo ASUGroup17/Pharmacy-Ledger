f88931a6644ba6fa337756a43a7fa231d860f172
Terin Champion <terinchampion@gmail.com>
2018-10-20 23:12:51 -0700

    US34 Set up Native Base and tested a button in App.js.  Emulator runs and shows button.

--- App.js
+++ App.js
@@ -7,7 +7,8 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet} from 'react-native';
+import { Container, Header, Content, Button, Text } from 'native-base'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,11 +21,16 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Container>
+        <Header><Text>Pharmacy Ledger</Text></Header>
+        <Content>
+        <Button bordered>
+          <Text>
+            Login!
+          </Text>
+        </Button>
+        </Content>
+      </Container>
     );
   }
 }
