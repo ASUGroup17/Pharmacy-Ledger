3a41f1e07108a6518b55d8f6d4e7d1e1c192e2d2
Terin Champion <terinchampion@gmail.com>
2018-10-21 11:39:08 -0700

    US4: Created login screed with a form to capture User Name and Password, Login Button and text from Prototype.

--- components/loginPage.js
+++ components/loginPage.js
@@ -7,20 +7,33 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet} from 'react-native';
-import { Container, Header, Content, Button, Text } from 'native-base'
+import {Platform, StyleSheet, Link, View} from 'react-native';
+import { Container, Header, Content, Button, Text, Form, Item, Input } from 'native-base'
 
 class LoginPage extends Component {
   render() {
     return (
       <Container style={styles.container}>
         <Header><Text>Pharmacy Ledger</Text></Header>
-        <Content style={styles.content}>
-        <Button bordered style={styles.button}>
-          <Text>
-            Login!
-          </Text>
-        </Button>
+        <Content>
+        <View style={styles.content}>
+          <Form>
+            <Item>
+              <Input placeholder="Username" />
+            </Item>
+            <Item last>
+              <Input placeholder="Password" />
+            </Item>
+          </Form> 
+          <Button bordered style={styles.button}>
+            <Text>
+              Login!
+            </Text>
+          </Button>
+          <Text style={styles.text}>Selected EMR</Text>
+          <Text style={styles.text}>PCH - Allscripts</Text>
+          <Text style={styles.link}>Change System</Text>
+          </View>
         </Content>
       </Container>
     );
@@ -31,20 +44,37 @@ export default LoginPage;
 
 const styles = StyleSheet.create({
   container: {
-    //flex: 1,
-    //justifyContent: 'center',
-    //alignItems: 'center',
+    flex: 1,
+    justifyContent: 'space-around',
+    alignContent: 'space-between',
+    //alignItems: 'center'
     //backgroundColor: '#F5FCFF',
   },
   content: {
     //fontSize: 20,
     //textAlign: 'center',
     //margin: 10,
+    //alignSelf: 'center',
+    alignContent: 'space-around',
     justifyContent: 'space-around'
   },
   button: {
+    alignSelf: 'center',
+    justifyContent: 'space-around',
+    alignContent: 'space-around',
+    marginBottom: 100,
+    marginTop: 50
     //textAlign: 'center',
     //color: '#333333',
     //marginBottom: 5,
   },
+  text: {
+    alignSelf: 'center',
+    padding: 2
+  },
+  link: {
+    alignSelf: 'center',
+    color: '#0000ff',
+    padding: 2
+  }
 });
