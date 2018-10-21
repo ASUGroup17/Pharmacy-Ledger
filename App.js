8cdeae807f782c43e35b88926c1cf581e5df8e72
Terin Champion <terinchampion@gmail.com>
2018-10-21 10:07:06 -0700

    US4 Created component loginPage and added this component to the App.js file.

--- App.js
+++ App.js
@@ -7,8 +7,8 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet} from 'react-native';
-import { Container, Header, Content, Button, Text } from 'native-base'
+import {Platform, StyleSheet, View} from 'react-native';
+import { loginPage } from './components/loginPage';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,35 +21,10 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <Container>
-        <Header><Text>Pharmacy Ledger</Text></Header>
-        <Content>
-        <Button bordered>
-          <Text>
-            Login!
-          </Text>
-        </Button>
-        </Content>
-      </Container>
+      <View>
+        <loginPage></loginPage>
+      </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
