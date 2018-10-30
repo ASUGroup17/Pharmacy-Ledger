dd1bdb47a2fbe4ed88e846688867d798ac8bbf20
Terin Champion <terinchampion@gmail.com>
2018-10-29 22:21:22 -0700

    US6 Tasks 36, 21, 25, 23 created test fields that show Patient ID, Patient ID label, Capture Image Button, Continue Button and Scan Patient's Wristband header.

--- components/PatientCapturePage.js
+++ components/PatientCapturePage.js
@@ -1,14 +1,56 @@
 import React, {Component} from 'react';
-import { View, Text } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import { Container, Content, Button, Text, Form, Item, Input } from 'native-base'
 
 class PatientCapturePage extends Component {
     render () {
         return (
-            <View>
-                <Text>On PatientCapturePage!!!</Text>
-            </View>
+            <Container style={styles.containerStyle}>
+                <Content>
+                <View style={styles.contentStyle}>
+                    <Text>
+                        Scan Patient's Wristband
+                    </Text>
+                    <Button bordered>
+                        <Text>
+                            Capture Image
+                        </Text>
+                    </Button>
+                    <View style={styles.patientIdView}>
+                        <Text>
+                            Patient ID:
+                        </Text>
+                        <Input placeholder="Patient ID" />
+                    </View>
+                    <Button bordered>
+                        <Text>
+                            Continue
+                        </Text>
+                    </Button>
+                </View>
+                </Content>
+            </Container>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    containerStyle: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between'
+    },
+    contentStyle: {
+        //flexDirection: 'column',
+        //flex: 1,
+        //alignContent: 'center',
+        //justifyContent: 'space-around'
+    },
+    patientIdView: {
+        flexDirection: 'row'
+    }
+
+
+})
 
 export default PatientCapturePage;
