d114364b252c999386e91d19bec250f445c95a4c
Kevin Nolf <knolf@asu.edu>
2018-10-31 17:51:29 -0400

    Creating SearchResultsPage that is not currently reachable due to errors in Navigation. SearchResultsPage is not being registered properly, cannot fix at the moment

--- components/SearchLedgerPage.js
+++ components/SearchLedgerPage.js
@@ -12,10 +12,30 @@
 //Importing different types of components to be used
 import React, { Component } from 'react';
 import  { Platform, StyleSheet, Link, View} from 'react-native';
-import { Container, Header, Content, Button, Text, Form, Item, Input } from 'native-base';
+import { Container, Content, Button, Text, Form, Item, Input } from 'native-base';
+
+
+/**
+ * These two lines were meant to help implement some navigation from the SearchLedgerPage to the SearchResultsPage.
+ * The SearchLedgerPage is not registered, and it's causing errors when the Submit button is selected
+ * 
+ */
+//import { Navigation } from 'react-native-navigation';
+//Navigation.registerComponent('components.SearchResultsPage', () => SearchResultsPage);
 
 
 class SearchLedgerPage extends Component {
+    //Creating handler to navigate to the Search Results Screen
+    /*searchResultsHandler = () => {
+        this.props.navigator.push({
+            screen: 'pharmacy-ledger.SearchResultsPage',
+            title: 'Search Results'
+        })
+
+
+         onPress={this.searchResultsHandler}>
+    }*/
+
     render () {
         return (
             <Container style={styles.Container}>
---
+++ components/SearchResultsPage.js
@@ -0,0 +1,101 @@
+/**
+ * Author: Kevin Nolf
+ * Start Date: 10/30/2018
+ * Description: Display the results of a Search from the Search Ledger Page
+ */
+
+ "use Strict";
+
+ //Importing Components
+ import React, { Component } from 'react';
+ import { Platform, StyleSheet, Link, View} from 'react-native';
+ import { Container, Header, Content, Button, Text, Form, Item, Input } from 'native-base';
+ 
+
+class SearchResultsPage extends Component {
+    
+    
+    //Creating Handler to Navigate the 'New Search' button, will nagivate to the SearchLedgerPage.js screen
+    newSearchHandler = () => {
+        this.props.navigator.push({
+            screen: 'pharmacy-ledger.SearchLedgerPage',
+            title: 'New Search'
+        })
+    }   
+
+     render () {
+         return (
+             <Container style={styles.Container}>
+                <Content>
+                    <View style={styles.Container}>
+                        <Form>
+                            <Item>
+                                <Input placeholder = "Administering Physician"/>
+                            </Item>
+                            <Item>
+                                <Input placeholder = "Date Administered"/>
+                            </Item>
+                            <Item>
+                                <Input placeholder = "Medication Name"/>
+                            </Item>
+                            <Item>
+                                <Input placeholder = "Lot #"/>
+                            </Item>
+                            <Item>
+                                <Input placeholder = "Concentration"/>
+                            </Item>
+                            <Item last>
+                                <Input placeholder = "Expiration Date"/>
+                            </Item>
+                        </Form>
+
+                        <Button bordered style = {styles.button}>
+                            <Text>
+                                Clear Search Results
+                            </Text>
+                        </Button>
+
+                        <Button bordered style = {styles.button} onPress={this.newSearchHandler} >
+                            <Text>
+                                New Search
+                            </Text>
+                        </Button>
+
+                    </View>
+                </Content>
+             </Container>
+         );
+     }
+ }
+
+ const styles = StyleSheet.create({
+     container: {
+         flex: 1,
+         justifyContent: 'space-around',
+         alignContent: 'space-between',
+     },
+     content: {
+        justifyContent: 'space-around',
+        alignContent: 'space-between',
+     },
+     button: {
+         alignSelf: 'center',
+         justifyContent: 'space-around',
+         alignContent: 'space-around',
+         marginBottom: 100,
+         marginTop: 50
+     },
+
+     text: {
+         alignContent: 'center',
+         padding: 2
+     },
+
+     link: {
+         alignSelf: 'center',
+         color: '#0000ff',
+         padding: 2
+     }
+ });
+
+ export default SearchResultsPage;
