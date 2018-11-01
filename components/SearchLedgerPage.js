c40a2a88e957fa326e73651a126650dc7a04e469
Kevin Nolf <knolf@asu.edu>
2018-10-31 22:40:29 -0400

    Removing some unneeded LOC from file after troubleshooting

--- components/SearchLedgerPage.js
+++ components/SearchLedgerPage.js
@@ -13,16 +13,6 @@
 import React, { Component } from 'react';
 import  { Platform, StyleSheet, Link, View} from 'react-native';
 import { Container, Content, Button, Text, Form, Item, Input } from 'native-base';
-import SearchResultsPage from './SearchResultsPage';
-
-
-/**
- * These two lines were meant to help implement some navigation from the SearchLedgerPage to the SearchResultsPage.
- * The SearchLedgerPage is not registered, and it's causing errors when the Submit button is selected
- * 
- */
-import { Navigation } from 'react-native-navigation';
-Navigation.registerComponent('components.SearchResultsPage', () => SearchResultsPage);
 
 
 class SearchLedgerPage extends Component {
