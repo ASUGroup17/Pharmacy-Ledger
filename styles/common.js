746c3dfddaba7bee755c24eb725740624743d60d
Jenalle Wentworth <jenallewentworth@gmail.com>
2018-11-16 23:58:18 -0800

    Created common styles

--- styles/common.js
+++ styles/common.js
@@ -87,7 +87,6 @@ export const medicationCaptureStyles = StyleSheet.create({
         flexGrow: 1,
         //alignItems: 'center',
         justifyContent: 'space-around',
-
     },
     preview: {
         flex: 1,
@@ -226,4 +225,116 @@ export const searchResultsPageStyles = StyleSheet.create({
         color: '#0000ff',
         padding: 2
     }
+});
+
+
+export const commonStyles = StyleSheet.create({
+
+  // confirmationPageStyles
+  // medicationCaptureStyles
+  // patientCapturePageStyles
+  containerStyle: {
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      flexGrow: 1
+  },
+
+  // loginPageStyles
+  // searchLedgerPageStyles
+  // searchResultsPageStyles
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignContent: 'space-between',
+  },
+
+  // confirmationPageStyles
+  contentStyle: {
+    flex: 1,
+    flexGrow: 1,
+  },
+
+  // medicationCaptureStyles
+  // patientCapturePageStyles
+  contentStyle2: {
+    flex: 1,
+    flexGrow: 1,
+    justifyContent: 'space-around',
+  },
+
+  // searchLedgerPageStyles
+  // loginPageStyles
+  content: {
+    alignContent: 'space-around',
+    justifyContent: 'space-around'
+  },
+
+  // searchResultsPageStyles
+  content2: {
+    justifyContent: 'space-around',
+    alignContent: 'space-between',
+  },
+
+  // medicationCaptureStyles
+  // patientCapturePageStyles
+  buttonStyle: {
+    alignSelf: 'center'
+  },
+
+  // searchLedgerPageStyles
+  // searchResultsPageStyles
+  // loginPageStyles
+  button: {
+    alignSelf: 'center',
+    justifyContent: 'space-around',
+    alignContent: 'space-around',
+    marginBottom: 100,
+    marginTop: 50
+  },
+
+  // loginPageStyles
+  // searchLedgerPageStyles
+  // searchResultsPageStyles
+  text: {
+    alignSelf: 'center',
+    padding: 2
+  },
+
+  // medicationCaptureStyles
+  preview: {
+    flex: 1,
+    flexGrow: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+
+  // patientCapturePageStyles
+  preview2: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+
+  // medicationCaptureStyles
+  // patientCapturePageStyles
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20
+  },
+
+  // loginPageStyles
+  // searchLedgerPageStyles
+  // searchResultsPageStyles
+  link: {
+    alignSelf: 'center',
+    color: '#0000ff',
+    padding: 2
+  }
+
 });
