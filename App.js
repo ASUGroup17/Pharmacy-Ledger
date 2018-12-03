a6731b419a4f6415904b28df906f56d5137ab8ab
Jenalle Wentworth <jenallewentworth@gmail.com>
2018-12-02 16:04:03 -0800

    US113 Task166 Move all screens to index file

--- App.js
+++ App.js
@@ -8,17 +8,12 @@
 
 import React, { Component } from 'react'
 import { Platform, StyleSheet, View } from 'react-native'
-import LoginPage from './components/LoginPage'
 import { Navigation } from 'react-native-navigation'
 import { Provider } from 'react-redux'
 import configureStore from './store/configureStore'
-import PatientCapturePage from './components/PatientCapturePage'
-import SearchLedgerPage from './components/SearchLedgerPage'
-import MedicationCapturePage from './components/MedicationCapturePage'
-import ConfirmationPage from './components/ConfirmationPage'
-import SearchResultsPage from './components/SearchResultsPage'
 // added us115 11/24 -cal
 import SplashScreen from 'react-native-splash-screen'
+import { registerComponents } from './components'
 
 const store = configureStore()
 
@@ -30,13 +25,7 @@ const store = configureStore()
 //   Provider
 // );
 
-// Register Screens
-Navigation.registerComponent('pharmacy-ledger.LoginPage', () => LoginPage)
-Navigation.registerComponent('pharmacy-ledger.PatientCapturePage', () => PatientCapturePage)
-Navigation.registerComponent('pharmacy-ledger.SearchLedgerPage', () => SearchLedgerPage)
-Navigation.registerComponent('pharmacy-ledger.MedicationCapturePage', () => MedicationCapturePage)
-Navigation.registerComponent('pharmacy-ledger.ConfirmationPage', () => ConfirmationPage)
-Navigation.registerComponent('pharmacy-ledger.SearchResultsPage', () => SearchResultsPage)
+registerComponents()
 
 // Start App
 Navigation.startSingleScreenApp({
