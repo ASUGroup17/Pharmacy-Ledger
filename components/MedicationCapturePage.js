4d3e577c6dfb3ca1aa3d63fc89ee7ac60618d1f5
Kevin Nolf <kevnolf@gmail.com>
2019-01-17 10:18:18 -0500

    US179 Tasks 195: Creating Action Items for Lot Number being captured. 197: Implement logic to parse a captured LotNumber.  This logic could be improved and probably will change a bit

--- components/MedicationCapturePage.js
+++ components/MedicationCapturePage.js
@@ -69,6 +69,7 @@ class MedicationCapturePage extends Component {
         
     }
 
+
     //Creates a match when passed the ndc number, the keyword, the field we are searching for
     // and the two word elements involved in the match and adds to DB.
     createMatch = (ndc, keyword, findField, keywordElement, findFieldElement) => {
@@ -112,33 +113,35 @@ class MedicationCapturePage extends Component {
             }
         }, this);
         
-        
+        //This for statement will continually loop through captured textBlocks, initially it will check for Lot Number, then Expiration Date
         for (var index = 0; index < capturedArray.length; index++){
             //Testing to see if 'lot' was captured AND if the next value in the line was captured, it will throw error otherwise
-            if ((capturedArray[index].word.toLowerCase() === "lot" || capturedArray[index].word.toLowerCase() === "batch")
-                     && (capturedArray.length > 1/*[index+1].word*/ && capturedArray[index+1].word.length > 6)){
-                console.log("Kevin: IT EQUALS LOT");
-                this.props.onLotNumberCapture(capturedArray[index+1].word);                
-                //From keyword(lot, batch) to data captured
-                let vertical_Difference = relative_Vertical_Distance(capturedArray[index].height, capturedArray[index+1].height,
-                    capturedArray[index].yCoord, capturedArray[index+1].yCoord );
-                let horizontal_Difference = relative_Horizontal_Distance (capturedArray[index].width, capturedArray[index+1].width, 
-                    capturedArray[index].xCoord, capturedArray[index+1].xCoord);
-                console.log("Kevin: Test vert: " + vertical_Difference  + "    horiz: " + horizontal_Difference);
-            }/*
-            else if (capturedArray[index].word.toLowerCase() === "exp") {
-
+            if (/*!medication.lotNumber*/true) {
+                //Logic to determine if captured word is in a "proper" format; there is room for improvement on this logic
+                if ((capturedArray[index].word.toLowerCase() === "lot" || capturedArray[index].word.toLowerCase() === "batch")
+                        && (capturedArray.length > 1/*[index+1].word*/ && capturedArray[index+1].word.length > 6)){                    
+                    //This is what calls the LotNumberAction Item, with onLotNumberCapture
+                    this.props.onLotNumberCapture(capturedArray[index+1].word);                
+                    
+                    //From keyword(lot, batch) to data captured
+                    //The verticle distance is sometimes 'infinity', I think this may be due to when we are dividing by zero or a tiny decimal,
+                        // we may need some error handling with this
+                    let vertical_Difference = relative_Vertical_Distance(capturedArray[index].height, capturedArray[index+1].height,
+                        capturedArray[index].yCoord, capturedArray[index+1].yCoord );
+                    let horizontal_Difference = relative_Horizontal_Distance (capturedArray[index].width, capturedArray[index+1].width, 
+                        capturedArray[index].xCoord, capturedArray[index+1].xCoord);
+                    console.log("Kevin: Test vert: " + vertical_Difference  + "    horiz: " + horizontal_Difference);
+                }
+            }}    
+            relative_Vertical_Distance = (keyword_Height, data_Height, keyword_yCoord, data_yCoord) => {
+                return (keyword_Height + data_Height + (data_yCoord - keyword_yCoord)) / (data_yCoord - keyword_yCoord);
             }
-        */}
-
-        relative_Vertical_Distance = (keyword_Height, data_Height, keyword_yCoord, data_yCoord) => {
-            return (keyword_Height + data_Height + (data_yCoord - keyword_yCoord)) / (data_yCoord - keyword_yCoord);
-        }
-    
-        relative_Horizontal_Distance = (keyword_Width, data_Width, keyword_xCoord, data_xCoord) => {
-            return (keyword_Width + data_Width + (data_xCoord - keyword_xCoord)) / (data_xCoord - keyword_xCoord);
-        }
-     }
+        
+            relative_Horizontal_Distance = (keyword_Width, data_Width, keyword_xCoord, data_xCoord) => {
+                return (keyword_Width + data_Width + (data_xCoord - keyword_xCoord)) / (data_xCoord - keyword_xCoord);
+            }
+          }
+     
 
     
 
