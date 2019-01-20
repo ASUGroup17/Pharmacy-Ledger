c5417925a2b5a67f568693c2b13aafa451c39f0e
Kevin Nolf <kevnolf@gmail.com>
2019-01-19 23:29:40 -0500

    Adding and fixing some of the code progress I lost before for US 179 Task 221, locating an expiration date

--- components/ExpirationDateCapture.js
+++ components/ExpirationDateCapture.js
@@ -8,25 +8,27 @@ export const capturedExpiration = (capturedArray) => {
         
 
         //let expPat1 = new RegExp("[0-1][0-9](-| |/)[2][0][0-9][0-9]");  //ex: 03/2020    03-2020  03 2020 
-        let expPat1 = new RegExp(/^(0[123456789]|[10]|[11]|[12])([-]|[\s]|[/])\b([2][0][0-9][0-9])\b/);  //ex: 03/2020    03-2020  03 2020 
+        let expPat1 = new RegExp(/^(0[123456789]|10|11|12)([-]|[\s]|[/])\b([2][0][0-9][0-9])$\b/);  //ex: 03/2020    03-2020  03 2020 
         
         let expPat2 = new RegExp(/\b(?:jan(?:uary)?|feb(?:ruary)?|mar(?:ch)?|apr(?:il)?|may|jun(?:e)?|jul(?:y)?|aug(?:ust)?|sep(?:tember)?|oct(?:ober)?|nov(?:ember)?|dec(?:ember)?)([-]|[\s]|[/])[2][0][0-9][0-9]/);
         //ex:  feb/2021    mar 2022   march 2019   nov-2021
 
-        let expPat3 = new RegExp(/(0[123456789]|[10]|[11]|[12])/);  //ex:  12-    03   11/                    Should there be a character after the numbers? or just numbers?
+        let expPat3 = new RegExp(/(0[123456789]|10|11|12)/);  //ex:  12-    03   11/                    Should there be a character after the numbers? or just numbers?
         let expPat4 = new RegExp(/([2][0][0-9][0-9])/);
         //expPat5 is the same as expPat2 without the space or slash between month and year
         let expPat5 = new RegExp(/\b(?:jan(?:uary)?|feb(?:ruary)?|mar(?:ch)?|apr(?:il)?|may|jun(?:e)?|jul(?:y)?|aug(?:ust)?|sep(?:tember)?|oct(?:ober)?|nov(?:ember)?|dec(?:ember)?)[2][0][0-9][0-9]/);
         //expPat6 is only checking for written months
         let expPat6 = new RegExp(/\b(?:jan(?:uary)?|feb(?:ruary)?|mar(?:ch)?|apr(?:il)?|may|jun(?:e)?|jul(?:y)?|aug(?:ust)?|sep(?:tember)?|oct(?:ober)?|nov(?:ember)?|dec(?:ember)?)/);
-        let expPat7 = new RegExp(/\b(0[123456789]|[10]|[11]|[12])\b([-]|[\s]|[/])\b([0-9][0-9])\b/);  // ([0-9][0-9])
-        console.log("Kevin: regx1: " + expPat1.test(":05/2020")); 
-       
+        let expPat7 = new RegExp(/(0[123456789]|10|11|12)([-]|[\s]|[/])([0-9][0-9])$/);  // ([0-9][0-9])
+        //console.log("Kevin: regx1: " + expPat1.test(":05/2020")); 
+        
         
         
         
         if (capturedArray[index].word.toLowerCase() === "exp" || capturedArray[index].word.toLowerCase() === "expiry" || capturedArray[index].word.toLowerCase() === "expdate" ||
         capturedArray[index].word.toLowerCase() === "exp:" || capturedArray[index].word.toLowerCase() === "exp.date" || capturedArray[index].word.toLowerCase() === "expiry:") {
+            console.log("Kevin: regx1: " + expPat1.test("11/18")); 
+            console.log("Kevin: regx7: " + expPat7.test("11/18")); 
             if (capturedArray[index+1] !== undefined){
                 console.log("Kevin: word: " + capturedArray[index+1].word + "    " + capturedArray[index+1].word.length);                
                 
