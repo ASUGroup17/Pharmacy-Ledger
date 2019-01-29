f21108fedb6f23332db8748ba6715133f5e83b30
Kevin Nolf <kevnolf@gmail.com>
2019-01-29 18:44:52 -0500

    US228 Tasks 248, 249, 252: Added JSON values of 20 Patient's Information. Name, address, DOB, email, Patient ID & index.  Created bar codes to correspond to the Patient ID, and created a Word Document with these bar codes with the names and DOB for each bar code.

---
+++ research/FakePatientData.js
@@ -0,0 +1,322 @@
+export const dummy_Patient_Data = [
+    {
+      "patient_Id": 654178641236,
+      "name": {
+        "first": "Chase",
+        "last": "Sullivan"
+      },
+      "email": "chase.sullivan@undefined.us",
+      "phone": "(930) 543-3174",
+      "address": "994 Arkansas Drive, Bergoo, Texas, 9029",
+      "birth_Date": {
+        "month": 10,
+        "day": 17,
+        "year": 1944
+      },
+      "index": 0
+    },
+    {
+      "patient_Id": 6541784120873,
+      "name": {
+        "first": "Boyer",
+        "last": "Ortega"
+      },
+      "email": "boyer.ortega@undefined.me",
+      "phone": "(837) 495-3141",
+      "address": "249 Madeline Court, Boonville, Georgia, 5535",
+      "birth_Date": {
+        "month": 5,
+        "day": 11,
+        "year": 1974
+      },
+      "index": 1
+    },
+    {
+      "patient_Id": 3301852560870,
+      "name": {
+        "first": "Maura",
+        "last": "Rodgers"
+      },
+      "email": "maura.rodgers@undefined.com",
+      "phone": "(911) 526-2989",
+      "address": "325 Quentin Street, Lookingglass, Minnesota, 6702",
+      "birth_Date": {
+        "month": 5,
+        "day": 5,
+        "year": 1984
+      },
+      "index": 2
+    },
+    {
+      "patient_Id": 3210021560873,
+      "name": {
+        "first": "Jenifer",
+        "last": "Rocha"
+      },
+      "email": "jenifer.rocha@undefined.co.uk",
+      "phone": "(959) 446-2957",
+      "address": "635 Bowne Street, Allamuchy, Connecticut, 7183",
+      "birth_Date": {
+        "month": 11,
+        "day": 7,
+        "year": 1946
+      },
+      "index": 3
+    },
+    {
+      "patient_Id": 3301446301278,
+      "name": {
+        "first": "Hart",
+        "last": "Lowe"
+      },
+      "email": "hart.lowe@undefined.net",
+      "phone": "(840) 446-2965",
+      "address": "479 Otsego Street, Wanamie, New York, 7986",
+      "birth_Date": {
+        "month": 3,
+        "day": 6,
+        "year": 2001
+      },
+      "index": 4
+    },
+    {
+      "patient_Id": 3301499001453,
+      "name": {
+        "first": "Pittman",
+        "last": "Dickson"
+      },
+      "email": "pittman.dickson@undefined.io",
+      "phone": "(935) 476-3369",
+      "address": "324 Kay Court, Sunwest, South Carolina, 2848",
+      "birth_Date": {
+        "month": 6,
+        "day": 17,
+        "year": 1960
+      },
+      "index": 5
+    },
+    {
+      "patient_Id": 3354710001459,
+      "name": {
+        "first": "Perkins",
+        "last": "Mueller"
+      },
+      "email": "perkins.mueller@undefined.org",
+      "phone": "(955) 467-2263",
+      "address": "808 Kaufman Place, Greenwich, American Samoa, 7944",
+      "birth_Date": {
+        "month": 1,
+        "day": 12,
+        "year": 1970
+      },
+      "index": 6
+    },
+    {
+      "patient_Id": 6512811024754,
+      "name": {
+        "first": "Shauna",
+        "last": "Wheeler"
+      },
+      "email": "shauna.wheeler@undefined.ca",
+      "phone": "(858) 466-2441",
+      "address": "903 Bradford Street, Rivera, Wisconsin, 3683",
+      "birth_Date": {
+        "month": 12,
+        "day": 13,
+        "year": 2004
+      },
+      "index": 7
+    },
+    {
+      "patient_Id": 6412002698743,
+      "name": {
+        "first": "Haley",
+        "last": "Wade"
+      },
+      "email": "haley.wade@undefined.biz",
+      "phone": "(803) 449-2604",
+      "address": "786 Onderdonk Avenue, Nadine, Federated States Of Micronesia, 9867",
+      "birth_Date": {
+        "month": 6,
+        "day": 9,
+        "year": 1952
+      },
+      "index": 8
+    },
+    {
+      "patient_Id": 6863259102109,
+      "name": {
+        "first": "Marianne",
+        "last": "Potts"
+      },
+      "email": "marianne.potts@undefined.name",
+      "phone": "(867) 477-3191",
+      "address": "338 Flatlands Avenue, Oley, Michigan, 5910",
+      "birth_Date": {
+        "month": 9,
+        "day": 12,
+        "year": 2008
+      },
+      "index": 9
+    },
+    {
+      "patient_Id": 6852101052105,
+      "name": {
+        "first": "Delores",
+        "last": "Mcdaniel"
+      },
+      "email": "delores.mcdaniel@undefined.info",
+      "phone": "(975) 521-2431",
+      "address": "483 Liberty Avenue, Bluffview, Massachusetts, 791",
+      "birth_Date": {
+        "month": 1,
+        "day": 9,
+        "year": 2012
+      },
+      "index": 10
+    },
+    {
+      "patient_Id": 6512811052108,
+      "name": {
+        "first": "Elliott",
+        "last": "Graham"
+      },
+      "email": "elliott.graham@undefined.tv",
+      "phone": "(949) 581-2578",
+      "address": "972 Livingston Street, Caledonia, Oregon, 3995",
+      "birth_Date": {
+        "month": 1,
+        "day": 5,
+        "year": 1970
+      },
+      "index": 11
+    },
+    {
+      "patient_Id": 6412002111457,
+      "name": {
+        "first": "Garrison",
+        "last": "Dalton"
+      },
+      "email": "garrison.dalton@undefined.us",
+      "phone": "(913) 598-2096",
+      "address": "822 Eldert Street, Yogaville, Marshall Islands, 8016",
+      "birth_Date": {
+        "month": 6,
+        "day": 11,
+        "year": 1986
+      },
+      "index": 12
+    },
+    {
+      "patient_Id": 7212045632174,
+      "name": {
+        "first": "Pugh",
+        "last": "Sosa"
+      },
+      "email": "pugh.sosa@undefined.me",
+      "phone": "(989) 579-2942",
+      "address": "287 Arlington Place, Trexlertown, Indiana, 8095",
+      "birth_Date": {
+        "month": 5,
+        "day": 26,
+        "year": 1983
+      },
+      "index": 13
+    },
+    {
+      "patient_Id": 7212047851450,
+      "name": {
+        "first": "Cynthia",
+        "last": "Hogan"
+      },
+      "email": "cynthia.hogan@undefined.com",
+      "phone": "(964) 420-2731",
+      "address": "753 Woodruff Avenue, Edneyville, Ohio, 4756",
+      "birth_Date": {
+        "month": 4,
+        "day": 1,
+        "year": 1994
+      },
+      "index": 14
+    },
+    {
+      "patient_Id": 7212002111452,
+      "name": {
+        "first": "Brenda",
+        "last": "Jefferson"
+      },
+      "email": "brenda.jefferson@undefined.co.uk",
+      "phone": "(874) 529-2890",
+      "address": "129 Rugby Road, Nord, California, 8465",
+      "birth_Date": {
+        "month": 7,
+        "day": 1,
+        "year": 1988
+      },
+      "index": 15
+    },
+    {
+      "patient_Id": 0217410272143,
+      "name": {
+        "first": "Conway",
+        "last": "James"
+      },
+      "email": "conway.james@undefined.net",
+      "phone": "(936) 444-2388",
+      "address": "161 Taaffe Place, Fairlee, New Hampshire, 360",
+      "birth_Date": {
+        "month": 4,
+        "day": 26,
+        "year": 2017
+      },
+      "index": 16
+    },
+    {
+      "patient_Id": 0214857272149,
+      "name": {
+        "first": "Suarez",
+        "last": "Mccarthy"
+      },
+      "email": "suarez.mccarthy@undefined.io",
+      "phone": "(992) 494-3340",
+      "address": "298 Cheever Place, Selma, Nevada, 9326",
+      "birth_Date": {
+        "month": 10,
+        "day": 24,
+        "year": 2007
+      },
+      "index": 17
+    },
+    {
+      "patient_Id": 2721458030859,
+      "name": {
+        "first": "Peggy",
+        "last": "Keller"
+      },
+      "email": "peggy.keller@undefined.org",
+      "phone": "(831) 541-3201",
+      "address": "141 Fountain Avenue, Accoville, Virginia, 905",
+      "birth_Date": {
+        "month": 7,
+        "day": 3,
+        "year": 1953
+      },
+      "index": 18
+    },
+    {
+      "patient_Id": 2721204930853,
+      "name": {
+        "first": "Hendrix",
+        "last": "Dunn"
+      },
+      "email": "hendrix.dunn@undefined.ca",
+      "phone": "(828) 575-3492",
+      "address": "248 Celeste Court, Elwood, Maryland, 4211",
+      "birth_Date": {
+        "month": 7,
+        "day": 15,
+        "year": 1963
+      },
+      "index": 19
+    }
+  ]
